ef838b99457e754b688f3ed335464d10552a70f9
James Finlay <jtfinlay@ualberta.ca>
2015-02-10 19:33:48 +0000

    Time graph selector works!

--- Web/public/js/timelapse.js
+++ Web/public/js/timelapse.js
@@ -1,39 +1,64 @@
-function TimeLapse(idCanvas, width, height, data) {
-	this.id = idCanvas;
-	this.width = width;
-	this.height = height;
-	this.vis = d3.select(idCanvas);
-}
+function TimeLapse() {}
 
 TimeLapse.prototype = {
 	constructor: TimeLapse,
 	// TODO Documentation
-	setData: function(data) {
+	drawChart: function(idSVG, data) {
 		var that = this;
+		var vis = d3.select(idSVG);
 
 		nv.addGraph(function() {
   			var chart = nv.models.lineChart()
-                		.margin({left: 100})
-                		.useInteractiveGuideline(true)
                 		.transitionDuration(350)
-                		.showLegend(true)
-                		.showYAxis(true)
-                		.showXAxis(true);
+				.interactive(false)
+				.showLegend(false)
+                		.showXAxis(true)
+                		.showYAxis(true);
 			chart.xAxis
-				.axisLabel('Time (ms)')
+				.axisLabel('Time')
       				.tickFormat(d3.format(',r'));
 			chart.yAxis
-      				.axisLabel('Voltage (v)')
+      				.axisLabel('Customers')
       				.tickFormat(d3.format('.02f'));
 			data = that.sinAndCos();
 			
-			that.vis
+			vis
 				.datum(data)
 				.call(chart);
 
 			nv.utils.windowResize(function() { chart.update() });
 			return chart;
 		});
+	},
+	drawSelector: function(idSVG) {
+		
+		var vis = d3.select(idSVG);
+
+		var width = $(idSVG).width();
+		var height = $(idSVG).height();
+
+		var container = vis
+			.append('svg')
+			.attr('x', 60)
+			.attr('y', 20)
+			.attr('width', width-80)
+			.attr('height', height-57);
+		var drag = d3.behavior.drag()
+			.on('dragstart', function() { rectangle.style('opacity', 1)})
+			.on('drag', function () {
+				rectangle.attr('x', Math.max(0, Math.min(d3.event.x - container.attr('x'), container.attr('width')-4)));
+			})
+			.on('dragend', function() { rectangle.style('opacity', .4)});
+
+		var rectangle = container.append('rect')
+					 .attr('id', 'selector')
+					 .attr('width', 4)
+					 .attr('height', height-71)
+					 .attr('fill', 'blue')
+					 .attr('cursor', 'pointer')
+					 .style('opacity', .4)
+					 .call(drag);
+
 	},
 	/**************************************
  * Simple test data generator
@@ -44,9 +69,7 @@ sinAndCos: function() {
 
   //Data is represented as an array of {x,y} pairs.
   for (var i = 0; i < 100; i++) {
-    sin.push({x: i, y: Math.sin(i/10)});
-    sin2.push({x: i, y: Math.sin(i/10) *0.25 + 0.5});
-    cos.push({x: i, y: .5 * Math.cos(i/10)});
+    sin.push({x: i, y: 1+Math.sin(i/10)});
   }
 
   //Line chart data should be sent as an array of series objects.
@@ -55,17 +78,6 @@ sinAndCos: function() {
       values: sin,      //values - represents the array of {x,y} data points
       key: 'Sine Wave', //key  - the name of the series.
       color: '#ff7f0e'  //color - optional: choose your own line color.
-    },
-    {
-      values: cos,
-      key: 'Cosine Wave',
-      color: '#2ca02c'
-    },
-    {
-      values: sin2,
-      key: 'Another sine wave',
-      color: '#7777ff',
-      area: true      //area - set to true if you want this line to turn into a filled area chart.
     }
   ];
 }
