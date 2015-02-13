a098bcb8017ab57b46900caea661cb85463be154
JesseTucker <jesseDtucker@gmail.com>
2015-02-12 17:59:34 -0700

    Basic sender script

---
+++ WiFi/Collector/sender.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+BODY_START="\"data\":["
+BODY_END="]"
+
+block=""
+counter=0
+MAX=100
+
+send()
+{
+    #TODO::JT use curl to actually send this to something
+    toSend="$BODY_START $1 $BODY_END"
+    echo "TO SEND: $toSend"
+}
+
+while read line 
+do
+    block="$block $line"
+    count=$((counter + 1))
+    if [ $counter = MAX ]; then
+	# send this block, reset and carry on
+	send "$block"
+	block=""
+	counter=0
+    else
+        # if we are not sending append a comma
+        block="$block,"
+    fi
+done
+
+# strip comma at end
+block="${block%?}"
+# send anything that is left
+send "$block"
