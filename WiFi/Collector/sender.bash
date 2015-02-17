fce45235db7122ca1cf71a7b8f2422f7abe71856
JesseTucker <jesseDtucker@gmail.com>
2015-02-16 19:51:13 -0700

    Stripped duplicate data. Now only unique entries are sent

--- WiFi/Collector/sender.bash
+++ WiFi/Collector/sender.bash
@@ -17,26 +17,35 @@ MAX=100
 
 send()
 {
-    toSend="$BODY_START $1 $BODY_END"
-    curl --silent -H "Content-Type: application/json" --data "$toSend" "$DEST"
+    # we need to strip duplicates, easiest way to do this is sort -u
+    # However, that only reads from file. So we dump the contents to a temp file,
+    # sort and strip, then send the results.
+    arr=$1
+    echo "$arr" > "./tmp.txt"
+    arr="$(sort -u -b -f ./tmp.txt)"
+
+    lastChar="${arr: -1}"
+    if [ "$lastChar" = "," ];then
+	arr="${arr%?}"
+    fi
+
+    toSend="$BODY_START $arr $BODY_END"
+    
+    curl --silent -m 10 -H "Content-Type: application/json" --data "$toSend" "$DEST"
+    rm "./tmp.txt"
 }
 
 while read line 
 do
-    block="$block $line"
+    block="$block $line,"$'\n'
     counter=$((counter + 1))
     if (( "$counter" > "$MAX" )); then
 	# send this block, reset and carry on
 	send "$block"
 	block=""
 	counter=0
-    else
-        # if we are not sending append a comma
-        block="$block,"
     fi
 done
 
-# strip comma at end
-block="${block%?}"
 # send anything that is left
 send "$block"
