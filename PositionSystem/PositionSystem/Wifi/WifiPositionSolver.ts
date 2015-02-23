5ed2c5554d63607ea72ef1a4a2edfc1e1e7125a2
jesseDtucker <jesseDtucker@gmail.com>
2015-02-22 23:15:21 -0700

    Math WIP, not done yet but all calculations should be in

--- PositionSystem/PositionSystem/Wifi/WifiPositionSolver.ts
+++ PositionSystem/PositionSystem/Wifi/WifiPositionSolver.ts
@@ -13,18 +13,125 @@ export class WifiEntry {
 	}
 }
 
+interface WifiEntryPair {
+	first: WifiEntry;
+	second: WifiEntry;
+}
+
+class Point {
+	private m_x: number;
+	private m_y: number;
+
+	constructor(x, y){
+		this.m_x = x;
+		this.m_y = y;
+	}
+
+	public x(): number { return this.m_x; }
+	public y(): number { return this.m_y; }
+
+	public Distance(p: Point): number {
+		var xSquared = (this.x() - p.x()) * (this.x() - p.x());
+		var ySquared = (this.y() - p.y()) * (this.y() - p.y());
+		return Math.sqrt(xSquared + ySquared);
+	}
+
+	public AddVector(p: Point): Point {
+		return new Point(p.x() + this.x(), p.y() + this.y());
+	}
+
+	public SubtractVector(p: Point): Point {
+		return new Point( p.x() - this.x(), p.y() - this.y());
+	}
+
+	public Magnitude(): number {
+		return this.Distance(new Point(0, 0));
+	}
+
+	public MultVector(magnitude: number): Point {
+		return new Point(this.x() * magnitude, this.y() * magnitude);
+	}
+
+	public Normalize(): Point {
+		var m = this.Magnitude();
+		return new Point(this.x() / m, this.y() / m);
+	}
+}
+
+class Circle {
+	private m_center;
+	private m_radius;
+
+	constructor(radius: number, point: Point) {
+		this.m_center = point;
+		this.m_radius = radius;
+	}
+
+	public radius(): number { return this.m_radius; }
+	public center(): Point { return this.m_center; }
+
+	public FindIntersections(c: Circle): Point[] {
+		// see http://paulbourke.net/geometry/circlesphere/ for math explanation
+		// I use the same variables as there to make it easy to reference... sorry for the 1 letter variables
+		// I wanted it to be consistent with the source as it does a much better job of explaining the math
+		// than I would. P0 = this.center(), P1 = c.center()
+		var d = this.center().Distance(c.center());
+
+		if (d > this.radius() + c.radius()) {
+			return []; // no points intersect
+		}
+		if(d < Math.abs(this.radius() - c.radius()) {
+			return null; // all points intersect so no solution... these return values are rather arbitrary...
+		}
+
+		var a = (this.radius() * this.radius() - c.radius() * c.radius() + d * d) / (2.0 * d);
+		var b = d - a;
+		var h = this.radius() * this.radius() - a * a;
+		
+		// p2 = center + a * (p1-p0) / d
+		var p1MinusP0 = c.center().SubtractVector(this.center());
+		var p2 = this.center().AddVector(p1MinusP0.MultVector(a / d));
+
+		// Note the plus minus!
+		// x3 = x2 +- h * (y1 - y0) / d
+		// y3 = y2 +- h * (x1 - x0) / d
+
+		var x_term2 = h * (c.center().y() - this.center().y()) / d;
+		var y_term2 = h * (c.center().x() - this.center().x()) / d;
+
+		var p3 = new Point(p2.x() + x_term2, p2.y() + y_term2);
+		var p4 = new Point(p2.x() - x_term2, p2.y() - y_term2);
+
+		return [p3, p4];
+	}
+}
+
 interface Station {
 	id: string;
-	x: number;
-	y: number;
+	point: Point;
 }
 
 export class PositionSolver {
 
 	private m_db: mongodb.Db;
 
+	private m_stations:any;
+
 	constructor(db:mongodb.Db) {
 		this.m_db = db;
+
+		// TODO::JT store this in DB and retrieve. Just hacking together for the time being
+		this.m_stations = {
+			"vm-drone": {
+				id: "vm-drone", point: { x: 0, y: 0 }
+			},
+			"vm-drone2": {
+				id: "vm-drone2", point: { x: 0, y: 15 }
+			},
+			"vm-drone3": {
+				id: "vm-drone3", point: { x: 25, y: 0 }
+			}
+		}
 	}
 
 	public solveFor(macIds: string[]): void {
@@ -79,6 +186,109 @@ export class PositionSolver {
 	}
 
 	private CalculatePosition(mac: String, latestEntries: WifiEntry[]) {
+		// can't perform trilateration without at least 3 measurments
+		if (latestEntries.length > 3) {
+			var pairs = this.GenerateStationPairs(latestEntries);
+		}
+	}
 
+	private DeterminePoints(pairs:WifiEntryPair[]): Point[] {
+		var result: Point[] = [];
+		var self = this;
+
+		pairs.forEach(function (pair, index, arr) {
+			var p = self.DeterminePoint(pair);
+			if (p != null) {
+				result.push();
+			}
+		});
+
+		return result;
+	}
+
+	///
+	//	Free Space Path Loss Equation. dbm is in dBm and result is in meters.
+	//	Expected range of input is about -75 to -45 and expected output is
+	//	2 meters - 50 meters. However, equation should work outside that range, that
+	//	is just what I expect. See http://bit.ly/1ykAjbd for a graph of the relation.
+	///
+	private FSPL(dbm: number): number {
+
+		return 10 ^ ((27.55 - (20 * Math.log(2412) / Math.log(10)) + Math.abs(dbm)) / 20.0);
+	}
+
+	private GetStationLocation(stationName): Point {
+		var result = null;
+		var station:Station = this.m_stations[stationName];
+		if (station != null) {
+			result = new Point(station.point.x, station.point.y);
+		}
+
+		return result;
+	}
+
+	private VoteForPoint(points: Point[], entries: WifiEntry[]): Point {
+		var pointsCounter:number[] = []; // key: point, value: vote count
+		points.forEach(function (val, idx, arr) {
+			pointsCounter[idx] = 0;
+		});
+
+		var stationCircles: Circle[] = [];
+		entries.forEach(function(station, idx, arr) {
+
+		});
+
+		entries.forEach(function (entry, index, array): void {
+
+		});
+	}
+
+	private DeterminePoint(pair: WifiEntryPair, entries:WifiEntry[]): Point {
+		var result:Point = null;
+		var firstDist = this.FSPL(pair.first.strength);
+		var secondDist = this.FSPL(pair.second.strength);
+
+		var firstStationLocation = this.GetStationLocation(pair.first.stationId);
+		var secondStationLocation = this.GetStationLocation(pair.second.stationId);
+		var vecBetweenStations = firstStationLocation.SubtractVector(secondStationLocation);
+		
+		if (firstStationLocation != null && secondStationLocation != null) {
+			var distBetweenStations = vecBetweenStations.Magnitude();
+
+			// we have 1 of 2 cases. Either the point is so far from each station that there is no intersection of the circles
+			// and we need to choose a point in the direct line between the stations as a likely point. Case 2 is the circles
+			// are close enough to overlap and so there are 2 possible points, we will need to select one of the two points.
+			if (distBetweenStations > (firstDist + secondDist)) {
+				// too far apart for there to be overlap
+				var ratioFromFirst = (firstDist) / (firstDist + secondDist);
+				result = vecBetweenStations.MultVector(ratioFromFirst);
+			}
+			else {
+				var c1 = new Circle(firstDist, firstStationLocation);
+				var c2 = new Circle(firstDist, firstStationLocation);
+
+				var intersections = c1.FindIntersections(c2);
+				if (intersections != null) {
+					// need to select 1 of the 2 points... lets vote!
+				}
+			}
+		}
+		else {
+			console.log("Unable to retrieve position for either : " + pair.first.stationId + " or " + pair.second.stationId);
+		}
+
+		return result;
+	}
+
+	private GenerateStationPairs(entries: WifiEntry[]): WifiEntryPair[] {
+		var res:WifiEntryPair[] = [];
+
+		entries.forEach(function (val, index, array) {
+			array.slice(index + 1, array.length).forEach(function (second, index, array) {
+				res.push({ first: val, second: second });
+			});
+		});
+
+		return res;
 	}
 }
