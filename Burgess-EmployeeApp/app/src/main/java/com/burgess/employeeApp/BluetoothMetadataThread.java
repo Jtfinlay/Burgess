8e643b2ff8b0ee8182778b273c6ae9a21443760b
jesseDtucker <jesseDtucker@gmail.com>
2015-03-13 15:53:02 -0600

    Migrated project to Android Studio and fixed up package names

---
+++ Burgess-EmployeeApp/app/src/main/java/com/burgess/employeeApp/BluetoothMetadataThread.java
@@ -0,0 +1,65 @@
+package com.burgess.employeeApp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
+import android.os.PowerManager;
+
+public class BluetoothMetadataThread extends Thread {
+	private ArrayList<Result> mResults;
+	private BluetoothCollection mBTCollector;
+	private BluetoothSendMetaData mBTSender;
+
+	private Object mSyncToken = new Object();
+	private PowerManager.WakeLock mWakeLock;
+
+	public BluetoothMetadataThread (BluetoothManager bluetoothManager, WifiManager wifiManager, ConnectivityManager connMgr, MainActivity self)
+	{
+		mBTCollector = new BluetoothCollection(getStationMacs(), bluetoothManager, wifiManager, connMgr, self, this, mSyncToken);
+		mBTSender = new BluetoothSendMetaData();
+		
+		//runs cpu in background to transmit location data while phone is asleep
+		PowerManager pm = (PowerManager)self.getApplicationContext().getSystemService(Context.POWER_SERVICE);
+		mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "WakeLock");
+	}
+
+	public void run() {
+		while (true)
+		{
+			mWakeLock.acquire();
+
+			synchronized(mSyncToken)
+			{
+				try {
+					mResults = new ArrayList<Result>();
+					mBTCollector.startCollection(mResults);
+					mSyncToken.wait();
+					mBTSender.POST(mResults);
+					
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public void addResult(Result newResult) {
+		mResults.add(newResult);
+	}
+	
+	//get from database when setup
+	private HashMap<String, String> getStationMacs()
+	{
+		HashMap<String, String> stationMacs = new HashMap<String, String>();
+		stationMacs.put("00:19:5B:0E:4C:71", "bt-stn1");
+		stationMacs.put("00:19:5B:0E:4C:72", "bt-stn2");
+		stationMacs.put("E4:98:D6:63:1D:86", "iPad");
+		stationMacs.put("04:1E:64:C7:A2:15", "iPhone");
+		return stationMacs;
+	}
+}
