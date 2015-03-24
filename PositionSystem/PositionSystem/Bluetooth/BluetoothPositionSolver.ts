4c44a00755caae87edf449014a1b0c5b44610b38
jesseDtucker <jesseDtucker@gmail.com>
2015-03-24 15:16:14 -0600

    Fixing WS issues, consistency is important

--- PositionSystem/PositionSystem/Bluetooth/BluetoothPositionSolver.ts
+++ PositionSystem/PositionSystem/Bluetooth/BluetoothPositionSolver.ts
@@ -5,78 +5,78 @@ import constants = require('../Constants');
 import common = require('../Common');
 
 class Station {
-    constructor(
-        public id: string,
-        public point: common.Point) { }
+	constructor(
+		public id: string,
+		public point: common.Point) { }
 }
 
 export class PositionSolver {
 
 	private m_rawDB: mongodb.Db;
-    private m_posDB: mongodb.Db;
-    private m_btLocDB: mongodb.Db;
+	private m_posDB: mongodb.Db;
+	private m_btLocDB: mongodb.Db;
 
-    private m_stations: Station[];
+	private m_stations: Station[];
 
 	constructor(rawDB:mongodb.Db, posDB:mongodb.Db, btLocDB:mongodb.Db) {
-        this.m_rawDB = rawDB;
-        this.m_posDB = posDB;
-        this.m_btLocDB = btLocDB;
-        this.m_stations = [];
+		this.m_rawDB = rawDB;
+		this.m_posDB = posDB;
+		this.m_btLocDB = btLocDB;
+		this.m_stations = [];
 
-        var self = this;
+		var self = this;
 
-        this.getBluetoothStations(function (station) {
-            self.m_stations.push(station);
-        });
-    }
+		this.getBluetoothStations(function (station) {
+			self.m_stations.push(station);
+		});
+	}
 
-    public solveFor(entry: common.BluetoothEntry): void {
-        var position: common.PositionEntry;
-        var point: common.Point = this.getPoint(entry.stationId);
-        position = new common.PositionEntry('', point.x(), point.y(), 0, new Date(entry.time), entry.mac);
+	public solveFor(entry: common.BluetoothEntry): void {
+		var position: common.PositionEntry;
+		var point: common.Point = this.getPoint(entry.stationId);
+		position = new common.PositionEntry('', point.x(), point.y(), 0, new Date(entry.time), entry.mac);
 
-        this.m_posDB.collection(constants.POS_COLLECTION, function (err, collection) {
-            if (err) {
-                console.log('BluetoothSolver::Error opening position collection: ' + err);
-                return
-            }
-            collection.insert(position, function (error, result) {
-                if (error) {
-                    console.log('Error saving positions to DB : ' + error);
-                }
-            });
-        });
-    }
+		this.m_posDB.collection(constants.POS_COLLECTION, function (err, collection) {
+			if (err) {
+				console.log('BluetoothSolver::Error opening position collection: ' + err);
+				return
+			}
+			collection.insert(position, function (error, result) {
+				if (error) {
+					console.log('Error saving positions to DB : ' + error);
+				}
+			});
+		});
+	}
 
-    private getBluetoothStations(cb: (station: Station) => void): void {
-        var self = this;
+	private getBluetoothStations(cb: (station: Station) => void): void {
+		var self = this;
 
-        this.m_btLocDB.collection(constants.BLUETOOTHLOCATIONS_COLLECTION, function (err, collection) {
-            if (err) {
-                console.log('BluetoothLocations::Error opening collection: ' + err);
-                return
-            }
-            collection.find().toArray(function (err, stations) {
-                if (err) {
-                    console.log('BluetoothLocations::Error iterating over find : ' + err);
-                        return
-                    }
+		this.m_btLocDB.collection(constants.BLUETOOTHLOCATIONS_COLLECTION, function (err, collection) {
+			if (err) {
+				console.log('BluetoothLocations::Error opening collection: ' + err);
+				return
+			}
+			collection.find().toArray(function (err, stations) {
+				if (err) {
+					console.log('BluetoothLocations::Error iterating over find : ' + err);
+						return
+					}
 
-                stations.forEach(function (element, index, array) {
-                    var station: Station = new Station(element.name, new common.Point(element.x, element.y));
-                    cb(station);
-                });
-            });
-        });
-    }
+				stations.forEach(function (element, index, array) {
+					var station: Station = new Station(element.name, new common.Point(element.x, element.y));
+					cb(station);
+				});
+			});
+		});
+	}
 
-    private getPoint(stationID: string): common.Point {
-        var result: common.Point;
-        this.m_stations.forEach(function (station, index, array) {
-            if (station.id == stationID)
-                result = station.point;
-        });
-        return result;
-    }
+	private getPoint(stationID: string): common.Point {
+		var result: common.Point;
+		this.m_stations.forEach(function (station, index, array) {
+			if (station.id == stationID)
+				result = station.point;
+		});
+		return result;
+	}
 }
